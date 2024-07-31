77a81a437613b7fc3ffddaf2bb4166fba258ab83
ClasherV <vaibhavraikwar505@gmail.com>
2024-08-01 00:34:26 +0530

    Done

--- Rough.py
+++ Rough.py
@@ -1,10 +1,1 @@
-import numpy as np
-import matplotlib.pyplot as plt
-X1=np.random.randint(1,100,size=(250,))
-Y1=np.random.randint(1,100,size=(250,))
-size=range(1,60,5)
-colr=['r','b','c','m','g','k']
-plt.scatter(X1,Y1,s=size,c=colr)
-plt.xlabel("X values")
-plt.ylabel("Y values")
-plt.show()
+print("\e[31mThis text is red\e[0m")
