33db21766d62d0438a9cd7c6465cef0d2cd3a030
ClasherV <vaibhavraikwar505@gmail.com>
2024-12-20 21:06:23 +0530

    End

--- Concepts/Scripts/Scripts without Escape Character/26. OOPs/2. Inheritance/1. Single Inheritance/7.py
+++ Concepts/Scripts/Scripts without Escape Character/26. OOPs/2. Inheritance/1. Single Inheritance/7.py
@@ -1,21 +1,22 @@
 class Human:
-    def __init__(self):
+    def __init__(self,num_hearts):
         self.num_eyes=2
         self.num_nose=1
+        self.num_hearts=num_hearts
     def eat(self):
         print("I can Eat")
     def work(self):
         print("I can Work")
 class Male(Human):
-    def __init__(self,name):
-        super().__init__()
+    def __init__(self,name,hearts):
+        super().__init__(hearts)
         self.name=name
     def flirt(self):
         print("I can Flirt")
     def work(self):
         super().work()
         print("I can Code")
-male_1=Male("Vaibhav")
+male_1=Male("Vaibhav",1)
 print(male_1.name)
 print()
 male_1.eat()
@@ -24,6 +25,7 @@ male_1.flirt()
 print()
 print(male_1.num_eyes)
 print(male_1.num_nose)
+print(male_1.num_hearts)
 
 """
 O/p: I can Eat
@@ -32,5 +34,6 @@ O/p: I can Eat
      I can Flirt
 
      2
+     1
      1
 """
