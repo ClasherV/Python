9a8651a35f3f8bd870caeca5e0b3a813e2f02cd4
ClasherV <vaibhavraikwar505@gmail.com>
2025-02-24 16:40:13 +0530

    Shift

--- Concepts/Scripts/3. Operators/4. Logical Operators/Rough.py
+++ Concepts/Scripts/3. Operators/4. Logical Operators/Rough.py
@@ -1,14 +1,10 @@
-import random
-print("Enter 1. for Rock, 2. for Paper  and 3. for scissor")
-user_choice=int(input("Enter your choice!: "))
-if(user_choice>=3 or user_choice<0):
-    print("You have entered wrong choice!")
-else:
-    computer_choice=random.randint(0,2)
-    print(f"Computer choose: {computer_choice}")
-    if(user_choice==0 and computer_choice==1 or user_choice==1 and computer_choice==2 or user_choice==2 and computer_choice==0 ):
-        print("you Loose!")
-    elif(user_choice==0 and computer_choice==2 or user_choice==1 and computer_choice==0 or user_choice==2 and computer_choice==1 ):
-        print("you Win!")
-    elif(user_choice==computer_choice):
-        print("Tie")
+# # Implement Nand and Nor Gates
+ 
+# a=int(input("Enter the Number 1: "))
+# b=int(input("Enter the Number 2: "))
+# nand=not(a and b)
+# print(nand)
+
+x,y=input("Input X and Y: ").split()
+print(x)
+print(y)
---
+++ Concepts/Scripts/7. Lists/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+# a=[2,4,1,7]
+# a.sort()
+# print(a)
+# print(sorted(a))
+
+a=['Z','z','B','b','C','c']
+b=['Z','z','B','b','C','c']
+a.sort()
+print(a)
+print(sorted(b))
