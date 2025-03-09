e73a06a3c3edce21ff7f83c26b2225880e4e038b
ClasherV <vaibhavraikwar505@gmail.com>
2025-03-09 16:13:07 +0530

    PyCharm

--- Concepts/Scripts/12. Loops/1. For/for_else.py
+++ Concepts/Scripts/12. Loops/1. For/for_else.py
@@ -2,12 +2,12 @@
 # for i in tuple:
 #     print(i)
 # else:
-#     print("Executed Succesfully")
+#     print("Executed Successfully")
 
-tuple=(1,2,3,4,5)
-for i in tuple:
+tup = (1, 2, 3, 4, 5)
+for i in tup:
     print(i)
-    if i==3:
+    if i == 3:
         break
 else:
-    print("Executed Succesfully")
+    print("Executed Successfully")
